e1096899857df15b728b4311abecac12b8f0bf62
Chin-Wei Huang <cw.huang427@gmail.com>
2019-02-13 21:48:59 -0500

    Update download.py
    - using academictorrents to download
    - pickle issue with python3

--- download.py
+++ download.py
@@ -1,43 +1,43 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Mon Jan 15 13:31:59 2018
 @author: chinwei
+
+do `pip install academictorrents` before running this script
 """
 
-from urllib.request import urlretrieve
-import _pickle as pickle
+import pickle
 import gzip
 import os
+import sys
 import numpy as np
-
+import academictorrents as at
 
 if __name__ == '__main__':
 
-    
     import argparse
+
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--dataset', type=str, default='mnist', 
+    parser.add_argument('--dataset', type=str, default='mnist',
                         choices=['mnist'],
                         help='dataset name')
-    parser.add_argument('--savedir', type=str, default='datasets', 
+    parser.add_argument('--savedir', type=str, default='datasets',
                         help='directory to save the dataset')
-    
+
     args = parser.parse_args()
     print(args)
-    
+
     if not os.path.exists(args.savedir):
         os.makedirs(args.savedir)
-        
+
     if args.dataset == 'mnist':
-        path = 'http://deeplearning.net/data/mnist'
-        mnist_filename_all = 'mnist.pkl'
-        local_filename = os.path.join(args.savedir, mnist_filename_all)
-        urlretrieve(
-            "{}/{}.gz".format(path,mnist_filename_all), local_filename+'.gz')
-        fp = gzip.open(local_filename+'.gz','rb')
-        tr,va,te = pickle.load(fp, encoding='latin1')
-        np.save(open(local_filename+'.npy','wb'), (tr,va,te))
+        local_filename = at.get("323a0048d87ca79b68f12a6350a57776b6a3b7fb")
         
+        if sys.version_info < (3,0):
+            tr, va, te = pickle.load(gzip.open(local_filename, 'r'))
+        else:
+            tr, va, te = pickle.load(gzip.open(local_filename, 'r'), 
+                                     encoding='latin1')
         
+        os.remove(local_filename)
+        np.save("{}/mnist".format(args.savedir), (tr, va, te))
