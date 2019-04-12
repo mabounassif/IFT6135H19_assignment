8c6f1321fac393c852a895d1368e01f4a65e6546
Mahmoud Nassif <mabounassif@Mahmouds-MacBook-Air.local>
2019-04-11 22:50:34 -0400

    Uncomment plots

--- assignment3/density_estimation.py
+++ assignment3/density_estimation.py
@@ -16,18 +16,18 @@ import matplotlib.pyplot as plt
 # plot p0 and p1
 plt.figure()
 
-# empirical
-#  xx = torch.randn(10000)
-#  f = lambda x: torch.tanh(x*2+1) + x*0.75
-#  d = lambda x: (1-torch.tanh(x*2+1)**2)*2+0.75
-#  plt.hist(f(xx), 100, alpha=0.5, density=1)
-#  plt.hist(xx, 100, alpha=0.5, density=1)
-#  plt.xlim(-5,5)
-#  # exact
-#  xx = np.linspace(-5,5,1000)
-#  N = lambda x: np.exp(-x**2/2.)/((2*np.pi)**0.5)
-#  plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx))
-#  plt.plot(xx, N(xx))
+empirical
+xx = torch.randn(10000)
+f = lambda x: torch.tanh(x*2+1) + x*0.75
+d = lambda x: (1-torch.tanh(x*2+1)**2)*2+0.75
+plt.hist(f(xx), 100, alpha=0.5, density=1)
+plt.hist(xx, 100, alpha=0.5, density=1)
+plt.xlim(-5,5)
+# exact
+xx = np.linspace(-5,5,1000)
+N = lambda x: np.exp(-x**2/2.)/((2*np.pi)**0.5)
+plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx))
+plt.plot(xx, N(xx))
 
 
 ############### import the sampler ``samplers.distribution4''
@@ -111,18 +111,16 @@ print('training done.')
 ############### (1) plot the output of your trained discriminator
 ############### (2) plot the estimated density contrasted with the true density
 
+r = xx # evaluate xx using your discriminator; replace xx with the output
+plt.figure(figsize=(8,4))
+plt.subplot(1,2,1)
+plt.plot(xx,r)
+plt.title(r'$D(x)$')
 
-
-#  r = xx # evaluate xx using your discriminator; replace xx with the output
-#  plt.figure(figsize=(8,4))
-#  plt.subplot(1,2,1)
-#  plt.plot(xx,r)
-#  plt.title(r'$D(x)$')
-
-#  estimate = np.ones_like(xx)*0.2 # estimate the density of distribution4 (on xx) using the discriminator;
-#                                  # replace "np.ones_like(xx)*0." with your estimate
-#  plt.subplot(1,2,2)
-#  plt.plot(xx,estimate)
-#  plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx))
-#  plt.legend(['Estimated','True'])
-#  plt.title('Estimated vs True')
+estimate = np.ones_like(xx)*0.2 # estimate the density of distribution4 (on xx) using the discriminator;
+                                # replace "np.ones_like(xx)*0." with your estimate
+plt.subplot(1,2,2)
+plt.plot(xx,estimate)
+plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx))
+plt.legend(['Estimated','True'])
+plt.title('Estimated vs True')
